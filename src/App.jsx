5013ead4fea613e35384fffeabde06787367cfdb
Jamsheera M P <jamsheeramp2@gmail.com>
2025-10-03 15:57:11 +0530

    videos fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
 
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import Head from './components/Head'
 import Body from './components/Body'
+import MainContainer from './components/MainContainer'
+import WatchPage from './components/WatchPage'
 
+
+const appRouter = createBrowserRouter([{
+  path: "/",
+  element: <Body />,
+  children:[
+    {
+      path:"/",
+      element: <MainContainer/>
+    },
+    {
+      path:"/watch",
+      element:<WatchPage/>
+    }
+  ]
+}])
 const App = () => {
   return (
     <div className='h-screen flex flex-col'>
-<div className='fixed top-0 left-0 right-0 z-50'>
- <Head/>
-</div>
-   
-    <div className='flex flex-grow pt-14'>
-        <Body/>
-    </div>
-  
-    
+      <div className='fixed top-0 left-0 right-0 z-50'>
+        <Head />
+      </div>
+      <div className='flex flex-grow pt-14'>
+      <RouterProvider router={appRouter} />
+      
+        
+      </div>
+
+
 
       {
         /**
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,20 +1,23 @@
-import Sidebar from './Sidebar'
-import MainContainer from './MainContainer'
+import Sidebar from "./Sidebar";
+import MainContainer from "./MainContainer";
+import WatchPage from "./WatchPage";
+import { Outlet } from "react-router-dom";
 
 //
 //
-const Body = () =>{
-    return (
-        <div className='flex w-full h-[calc(100vh-56px)] overflow-y-auto'>
-            <div className='w-60   '>
- <Sidebar/>
-            </div>
-           <div className='flex flex-grow h-[calc(100vh-56px)] overflow-y-auto '>
-            <MainContainer/>
-           </div>
-           
-        </div>
-    )
-}
+const Body = () => {
+  return (
+    <div className="flex w-full h-[calc(100vh-56px)] overflow-y-auto">
+      <div className=" w-96   ">
+        <Sidebar />
+      </div>
+      <div className="flex flex-grow h-[calc(100vh-56px)] overflow-y-auto ">
+        <Outlet />
+        
+        
+      </div>
+    </div>
+  );
+};
 
-export default Body
+export default Body;
