b3987a16aa8ed207ab2b826bea67ce43172d3538
Jamsheera M P <jamsheeramp2@gmail.com>
2025-10-04 15:52:04 +0530

    an higher order component for ad videocard created

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -1,8 +1,21 @@
-
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { closeMenu } from "../redux/appSlice"
+import { useSearchParams} from "react-router"
 
 const WatchPage = () =>{
+
+    const [searchParams] = useSearchParams()
+    console.log(searchParams);
+    
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        dispatch(closeMenu())
+    },[])
     return (
-        <div></div>
+        <div>
+            
+        </div>
     )
 }
 
